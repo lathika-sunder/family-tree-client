983ff1b35bf45707e013fa1dfff2746387c1ab99
Lathika Sunder <lathikasunder11@gmail.com>
2023-12-23 11:07:24 +0530

    Add - Family Tree Component

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./HomePage.css";
 import CarouselComp from "../../components/CarouselComp/CarouselComp";
 import { TypeAnimation } from "react-type-animation";
-import { Parallax, Background } from 'react-parallax';
+import { Parallax, Background } from "react-parallax";
 import HeaderComp from "../../components/HeaderComp/HeaderComp";
 
 import QuoteComp from "../../components/QuoteComp/QuoteComp";
@@ -10,15 +10,18 @@ import FamilyTreeComp from "../../components/FamilyTreeComp/FamilyTreeComp";
 const HomePage = () => {
   return (
     <div className="home">
-      <HeaderComp/>
+      <HeaderComp />
       <div className="carousel-comp">
         <CarouselComp />
       </div>
-     <QuoteComp text="Families are like branches on a tree, we grow in different directions yet our roots remain as one." author=""/>
+      <QuoteComp
+        text="Families are like branches on a tree, we grow in different directions yet our roots remain as one."
+        author=""
+      />
 
-<div className="family-tree">
-<FamilyTreeComp/>
-</div>
+      <div className="family-tree">
+        <FamilyTreeComp />
+      </div>
     </div>
   );
 };
